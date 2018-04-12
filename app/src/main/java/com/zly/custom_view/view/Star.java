66cc1a040ab3ea8eff998b91edf2569056d12fc9
zly <zlydwyyx@126.com>
2018-04-12 17:16:59 +0800

    添加流动动画

--- app/src/main/java/com/zly/custom_view/view/Star.java
+++ app/src/main/java/com/zly/custom_view/view/Star.java
@@ -26,6 +26,11 @@ public class Star extends View {
     private int mCirWidth;
     private String TAG = "Star";
     private int mDegree = 0;
+    private int num1 = 9;
+    private int num2 = 1;
+    private int num3 = 1;
+    private int num4 = 1;
+    private int num5 = 1;
 
     public Star(Context context) {
         this(context, null);
@@ -38,9 +43,9 @@ public class Star extends View {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.star);
         mLineColr = typedArray.getColor(R.styleable.star_line_color, Color.parseColor("#000000"));
         mLineWidth = (int) typedArray.getDimension(R.styleable.star_line_width, 20);
-        mCirColor = typedArray.getColor(R.styleable.star_cir_color, Color.parseColor("#000000"));
-        mCirWidth = (int) typedArray.getDimension(R.styleable.star_cir_width, 30);
-        mCirRadius = (int) typedArray.getDimension(R.styleable.star_cir_radius, 50);
+        mCirColor = typedArray.getColor(R.styleable.star_cir_color, Color.parseColor("#00ff00"));
+        mCirWidth = (int) typedArray.getDimension(R.styleable.star_cir_width, 10);
+        mCirRadius = (int) typedArray.getDimension(R.styleable.star_cir_radius, 100);
         typedArray.recycle();
 
         //create the paint
@@ -64,29 +69,91 @@ public class Star extends View {
             Log.d(TAG, "zly --> getMeasureSize EXACTLY.");
             result = sizeSpec;
         } else {
-            Log.d(TAG, "zly --> getMeasureSize AT_MOST.");
-            result = mCirRadius<<1;
+            Log.d(TAG, "zly --> getMeasureSize AT_MOST mCirRadius:" + mCirRadius + " sizeSpec:" + sizeSpec);
+            result = (mCirRadius<<1) + mCirWidth;
             if(modeSpec == MeasureSpec.AT_MOST) {
                 result = Math.min(sizeSpec, result);
             }
         }
+        Log.d(TAG, "zly --> result:" + result);
         return result;
     }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
+//        Log.d(TAG, "zly --> onLayout left:" + left + " top:" + top + " right" +  right + " bottom:" + bottom);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.save();
-        canvas.translate(mCirRadius, mCirRadius);
-        mDegree += 10;
-        canvas.drawArc(-mCirRadius, mCirRadius, mCirRadius, -mCirRadius, 90, mDegree, false, mPaint);
-        if (mDegree >= 360) {
-            canvas.drawLine(-(float)(mCirRadius*Math.sin(toRadians(45))), -(float)(mCirRadius*Math.cos(toRadians(45))), 0, mCirRadius, mPaint);
+        canvas.translate(mCirRadius + mCirWidth/2, mCirRadius + mCirWidth/2);
+        canvas.drawArc(-mCirRadius, -mCirRadius, mCirRadius, mCirRadius, 90, mDegree, false, mPaint);
+        Log.d(TAG, "zly --> onDraw mDegree:" + mDegree + " num1:" + num1 + " num2:" + num2 + " num3:" + num3);
+
+        if (mDegree == 360) {
+//            Log.d(TAG, "zly --> X1:" + -computeX() + " Y1:" + (computeY()+mCirRadius) + " X2:" + (-computeX()/10*num1)
+//                + " Y2:" + ((computeY()+mCirRadius)/10*num1));
+            canvas.restore();
+            canvas.save();
+            canvas.translate(mCirRadius + mCirWidth/2, 0);
+            canvas.drawLine(-computeX(), computeY()+mCirRadius, -computeX()/10*num1,
+                    ((computeY()+mCirRadius)/10*num1), mPaint);
+        } else {
+            mDegree += 10;
         }
+
+        if (0 == num1) {
+            canvas.drawLine(0, 0, computeX()*num2/10, (computeY()+mCirRadius)*num2/10, mPaint);
+        } else if (360 == mDegree) {
+            num1--;
+        }
+
+        canvas.restore();
+
+        if (10 == num2) {
+            canvas.save();
+            canvas.translate(mCirRadius + computeX() + mCirWidth/2, mCirRadius + computeY() + mCirWidth/2);
+            canvas.drawLine(0, 0, -computeX()*2/10*num3, -computeY()*2/10*num3, mPaint);
+            canvas.restore();
+        } else if (0 == num1) {
+            num2++;
+        }
+
+        if (10 == num3) {
+            canvas.save();
+            canvas.translate(mCirRadius-computeX(), mCirRadius-computeY());
+            canvas.drawLine(0, 0, computeX()*2/10*num4, 0, mPaint);
+            canvas.restore();
+        } else if (10 == num2) {
+            num3++;
+        }
+
+        if (10 == num4) {
+            canvas.save();
+            canvas.translate(mCirRadius + computeX(), mCirRadius - computeY());
+            canvas.drawLine(0, 0, -computeX()*2/10*num5, computeY()*2/10*num5, mPaint);
+            canvas.restore();
+        } else if (10 == num3) {
+            num4++;
+        }
+
+        if (num5 < 10 && 10 == num4) {
+            num5++;
+        }
+
+        if (mDegree <= 360 || num1 >= 0 || num2 <= 10 || num3 <= 10 || num4 <= 10 || num5 <= 10) {
+            postInvalidateDelayed(100);
+        }
+    }
+
+    private float computeX() {
+        return (float) (mCirRadius*Math.cos(Math.toRadians(45)));
+    }
+
+    private float computeY() {
+        return (float) (mCirRadius*Math.sin(Math.toRadians(45)));
     }
 }
